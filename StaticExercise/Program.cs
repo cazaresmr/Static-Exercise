b8bed2ae1bc59060431c03ea5dfd5ccff2b842af
Michael Cazares <michaelcazares@gmail.com>
2024-07-27 19:15:24 -0700

    completed exercise

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace StaticExercise
+﻿using System;
+
+namespace StaticExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double fahrenheit = 98.6;
+            double celsius = TempConverter.FahrenheitToCelsius(fahrenheit);
+            Console.WriteLine($"{fahrenheit} Fahrenheit is {celsius} Celsius");
+
+            celsius = 37;
+            fahrenheit = TempConverter.CelsiusToFahrenheit(celsius);
+            Console.WriteLine($"{celsius} Celsius is {fahrenheit} Fahrenheit");
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    public static class TempConverter { 
+    
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            return (fahrenheit - 32) * 5.0 / 9.0;
+        }
+
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * 9.0 / 5.0) + 32;
+        }
+    }
+}
+
